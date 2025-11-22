936411eaedd466e70961b87d9771216275f83984
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2025-11-22 18:17:57 +0530

    Update

--- modules/video/controllers/getVData.js
+++ modules/video/controllers/getVData.js
@@ -22,6 +22,7 @@ const getvideoData = async (req, res) => {
     res.status(200).json({
         status: "success",
         data: getvideoData,
+        link:"https://kalaxhunter.vercel.app/en/video?id="+getvideoData._id,
     });
 };
 
